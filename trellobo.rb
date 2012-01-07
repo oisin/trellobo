ea38d212e03a77bcc6a863ee1f156f28f4943b91
Oisin Hurley <oisin.hurley@gmail.com>
2012-01-07 17:59:35 +0000

    Typo

--- trellobo.rb
+++ trellobo.rb
@@ -13,7 +13,7 @@ bot = Cinch::Bot.new do
   configure do |c|
     c.server = "irc.freenode.org"
     c.nick = "trellobot"
-    c.channels = ["#dubinjs"]
+    c.channels = ["#dublinjs"]
     sync_board "https://api.trello.com/1/boards/4f05b412cf33c09c016a90df/lists"
   end
 
