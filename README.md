4b89a125edae92a720cf5f6734087001c3bd5ff1
Oisin Hurley <oisin.hurley@gmail.com>
2012-01-08 17:11:00 +0000

    Updates to readme; Clean up bot code and addition of sync, help commands;

--- README.md
+++ README.md
@@ -1,5 +1,52 @@
 ## Trellobo
-This is a simple IRC bot written using [cinch](http://github.com/cinchrb/cinch). The idea is that people using the *dublinjs* IRC channel can address the bot to get a list of the current ideas for talks that are stored as cards in a Trello board. 
-## Implementation Notes
+This is a simple IRC bot written using [cinch](http://github.com/cinchrb/cinch). The motivation for writing this was that people using the *dublinjs* IRC channel can address the bot to get a list of the current ideas for talks that are stored as cards in a Trello board. 
 
-Use *trellobot: ideas* to get a list of the cards in the Ideas list on the Dublin JS Trello board.
+This incarnation is generalized a little, and it can serve as a (simple) general purpose IRC bot that will communicate with Trello. Configuration of API key, channel name and URLs are driven thru ENV. 
+
+The Trello API is used in readonly mode in this code, so all you need to access is your developer key, providing the board is public. Trellobot simply uses [rest-client](https://github.com/archiloque/rest-client) for this purpose.
+
+At some point in Trellobot's life, it will gain the capability to add cards to lists on a Trello board. That needs a little more development, with addition of Oauth capability to the Trellobot server.
+
+Trellobot understands the commands *help* and *sync*. Anything else it interprets as the name of a list on the target board that has been configured. If you send a private message containing *quit*, then Trellobot will disconnect from the server (and exit). Depending on how you have it deployed, it may very well respawn at that point.
+
+## Environment Vars
+Trellobot needs the following in the ENV to operate:
+<table>
+  <tr><th>ENV</th><th>Content</th></tr>
+  <tr><td>TRELLO\_API\_KEY</td><td>Your Trello developer API key</td></tr>
+  <tr><td>TRELLO\_BOARD\_API_URL</td><td>The API URL for your board</td></tr>
+  <tr><td>TRELLO\_BOARD\_HUMAN_URL</td><td>The human-friendly URL for your board</td></tr>
+  <tr><td>TRELLO\_BOT\_CHANNEL</td><td>The channel that trellobot should join</td></tr>
+</table>  
+
+## Where do I get the API URL for my board?
+I used the magic of _curl_ for this particular job of discovery. The Trello API covers a lot, [as you can read in the beta docs](https://trello.com/docs/api/index.html) and sometimes it's not immediately obvious where to grab on to find a particular piece of information. 
+
+If you know your username, you can list all your boards, with their ids. You then use the id to get API URL of the board. In this example, I'm listing all _public_ boards of the _dublinjavascript_ member and limiting the data returned on the boards to just the _name_ field (nice idea on the filtering guys) of the board. The id comes back for free, of course.
+
+<code>
+61%  curl https://api.trello.com/1/members/dublinjavascript?key=TRELLO\_API\_KEY\&boards=public\&board_fields=name
+{
+    "id": _the id of the member_,
+    "fullName": "Dublin JavaScript",
+    "username": "dublinjavascript",
+    "gravatar": _the gravatar id for the profile pic_,
+    "bio": "",
+    "url": "https://trello.com/dublinjavascript",
+    "boards": [
+        {
+            **"id": "4f05b412cf33c09c016a90df",**
+            "name": "Dublin Javascript Talks"
+        }
+    ]
+}
+</code>
+
+The boldfaced **id** up there is the piece you are looking for - put this into a URL like this
+
+<code>
+  https://api.trello.com/1/boards/**4f05b412cf33c09c016a90df**/lists
+</code>
+
+and that is your TRELLO\_BOARD\_API\_URL.
+
--- trellobo.rb
+++ trellobo.rb
@@ -2,38 +2,87 @@ require 'cinch'
 require 'rest_client'
 require 'json'
 
+# there are 4 environment variables that must be set for the trellobot to behave
+# the way he is supposed to - 
+#
+# TRELLO_API_KEY : your Trello API developer key
+# TRELLO_BOT_CHANNEL : the name of the channel you want trellobot to live on, the server is freenode
+# TRELLO_BOARD_API_URL : the trellobot looks at only one board and the lists on it, put the api url here
+# TRELLO_BOARD_HUMAN_URL : the trellobot will send the human here if he can't get the lists or cards
+
 $board = nil
 
-def sync_board(url)
-  data = RestClient.get url, {:params => {:key => "9b0e45ad3343361401c0465c5278a74d"}, :accept => "json"}
+def sync_board
+  data = RestClient.get ENV['TRELLO_BOARD_API_URL'], {:params => {:key => ENV['TRELLO_API_KEY']}, :accept => "json"}
   $board = JSON.parse(data)
+end
+
+def say_help(msg)
+  msg.reply "I can tell you the open cards on the lists on your Trello board. Just address me with the name of the list (it's not case sensitive)."
+  msg.reply "For example - trellobot: ideas"
+  msg.reply "I also understand the these commands : "
+  msg.reply "  -> 1. help - shows this!"
+  msg.reply "  -> 2. sync - resyncs my cache with the board."
 end
 
 bot = Cinch::Bot.new do
   configure do |c|
-    c.server = "irc.freenode.org"
+    c.server = "irc.freenode.net"
     c.nick = "trellobot"
-    c.channels = ["#dublinjs"]
-    sync_board "https://api.trello.com/1/boards/4f05b412cf33c09c016a90df/lists"
+    c.channels = [ENV['TRELLO_BOT_CHANNEL']]
+    sync_board
   end
 
-  on :message, /^trellobot/ do |m|
-    if m.message =~ /ideas/ 
-      ideas_list = $board.select { |l| "Ideas".eql?(l['name'])}
-      unless ideas_list.empty?
-        cards = ideas_list[0]['cards']
-        m.reply "I have about #{cards.count} ideas today"
-        cards.each { |i|
-          m.reply "  ->  #{i['name']}"
-        }
-      else
-        m.reply "Ideas? You're looking for ideas, #{m.user.nick}?."
-      end
-    else
-      m.reply "Ok, I don't know what that meant."
+  # trellobot is polite, and will only reply when addressed
+  on :message, /^trellobot[_]*:/ do |m|
+    # if trellobot can't get thru to the board, then send the human to the human url
+    sync_board unless $board
+    unless $board
+      m.reply "I can't seem to get the list of ideas from Trello, sorry. Try here: #{ENV['TRELLO_BOARD_HUMAN_URL']}" 
+      bot.halt
     end
+    
+    # trellobot: what up?  <- The bit we are interested in is past the ':'
+    parts = m.message.split(':',2)
+    searchfor = parts[1].strip.downcase
+    
+    case searchfor
+      when /help/
+        say_help(m)
+      when /sync/
+        sync_board
+        m.reply "Ok, synced the board, #{m.user.nick}."
+      else
+        if searchfor.length > 0
+          # trellobot presumes you know what you are doing and will attempt
+          # to retrieve cards using the text you put in the message to him
+          # at least the comparison is not case sensitive
+          list = $board.select { |l| searchfor.eql?(l['name'].strip.downcase)}
+          if list.empty?
+            m.reply "There's no list called <#{searchfor}> on the board, #{m.user.nick}. Sorry."
+          else
+            cards = list[0]['cards']
+            if cards.count == 0
+              m.reply "Nothing doing on that list today, #{m.user.nick}."
+            else
+              ess = (cards.count == 1) ? "" : "s"
+              m.reply "I have #{cards.count} card#{ess} today"
+              inx = 1
+              cards.each { |i|
+                m.reply "  ->  #{inx.to_s}. #{i['name']}"
+                inx += 1
+              }
+            end
+          end
+        else
+          say_help(m)
+        end
+    end 
   end
   
+  # if trellobot loses his marbles, it's easy to disconnect him from the server
+  # note that if you are doing a PaaS deploy, he may respawn depending on what 
+  # the particular hosting env is (e.g. Heroku will start him up again)
   on :private, /quit/ do |m|
     bot.quit
   end
